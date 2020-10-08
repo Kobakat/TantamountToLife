37de71315aabd93dab508457b3b23d910af83c1d
Kobakat <54965702+Kobakat@users.noreply.github.com>
2020-10-08 03:42:49 -0500

    adds basic character movement and camera control

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    Transform target = null;
+    [SerializeField]
+    float distFromPlayer, height;
+
+    void FixedUpdate()
+    {
+        StandardStateUpdate();
+        
+        this.transform.LookAt(target);
+    }
+
+    private void StandardStateUpdate()
+    {
+        Vector3 adjust = new Vector3(transform.position.x, target.position.y + height, transform.position.z);
+        transform.position = adjust;
+
+        Vector3 targetPos = target.position + (transform.position - target.position).normalized * distFromPlayer;
+        transform.position = targetPos;
+    }
+}
