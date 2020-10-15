ae5354ebb5dc8f478c4922c070a51b73b25e8f13
Lienau <380790@colum.edu>
2020-10-14 21:00:49 -0500

    Adds practice dummy that interacts with sword swings

---
+++ Assets/Scripts/Enemies/PlayerWeaponTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+/*HACK
+ *This is a demo script and will be replaced with a modular class structure mostly identical to the player's later
+ */
+
+public class PlayerWeaponTrigger : MonoBehaviour
+{
+    ParticleSystem ps;
+    Rigidbody rb;
+    void Start()
+    {
+        this.ps = GetComponent<ParticleSystem>();
+        this.rb = GetComponent<Rigidbody>();
+    }
+
+    public static event Action<ParticleSystem, Rigidbody> EnemyStruck;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Weapon"))
+        {
+            if (EnemyStruck != null)
+            {
+                EnemyStruck.Invoke(this.ps, this.rb);
+            }
+        }
+    }
+}
